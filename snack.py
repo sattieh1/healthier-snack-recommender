25d843a5c13afbcc078e9505491dce80bf43f638
Salman Attieh <sw.attieh@gmail.com>
2026-05-20 11:53:01 +0300

    Add and for last item in ingredients list

--- snack.py
+++ snack.py
@@ -9,13 +9,13 @@ class Snack:
 
     def __str__(self):
         return f"""This {self.product} from brand {self.brand} is better for you because it is {self.why_better} and has {self.calories} calories per 100 gram 
-The list of ingredients are {self.format_ingredients()}
+The list of ingredients are {self.format_ingredients()}.
 The macros are {self.format_macros()} """
     
     def format_macros(self):
         return ", ".join([f"{k}: {v}g" for k, v in self.macros.items()])
     def format_ingredients(self):
-        return  ", ".join(self.ingredients)
+        return f"{', '.join(self.ingredients[:-1])} and {self.ingredients[-1]}"
     
 if __name__ == "__main__":
     test_snack = Snack(
